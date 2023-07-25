c8644e71e673de4e5990d0275916dc847ec66ec3
Justine Pile <115434763+JustinePile@users.noreply.github.com>
2023-07-25 10:58:43 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,57 @@
+# Required imports for Flask
+from flask import Flask, request, jsonify
+import pandas as pd 
+
+# Create the Flask app
+app = Flask(__name__)
+
+# Read the CSV file into a pandas DataFrame
+file_path = 'data/MAL-anime.csv'
+df = pd.read_csv(file_path)
+
+# Define a function to create and train the linear regression model
+def train_linear_regression_model(X_train_encoded, y_train):
+    model = LinearRegression()
+    model.fit(X_train_encoded, y_train)
+    return model
+
+# Prepare the data for modeling
+X = df.drop(columns=['Score'])  # Features (all columns except 'Score')
+y = df['Score']  # Target variable
+
+# Combine the training and test sets to get all possible categorical values
+combined_data = pd.concat([X, X_test], axis=0)
+
+# Perform one-hot encoding on categorical columns with all possible values
+X_encoded = pd.get_dummies(combined_data)
+
+# Split the data into training and testing sets again
+X_train_encoded = X_encoded.iloc[:len(X_train)]
+X_test_encoded = X_encoded.iloc[len(X_train):]
+
+# Create and train the linear regression model using the function
+model = train_linear_regression_model(X_train_encoded, y_train)
+
+# Endpoint for making predictions
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get the JSON data from the request
+        data = request.get_json()
+
+        # Perform one-hot encoding on the input data
+        X_input = pd.DataFrame(data)  # Convert JSON to DataFrame
+        X_input_encoded = pd.get_dummies(X_input)
+
+        # Make predictions using the trained model
+        y_pred = model.predict(X_input_encoded)
+
+        # Return the predictions as JSON
+        return jsonify({'predictions': y_pred.tolist()})
+
+    except Exception as e:
+        return jsonify({'error': 'An error occurred: ' + str(e)}), 500
+
+# Run the Flask app on localhost:5000
+if __name__ == '__main__':
+    app.run(debug=True)
